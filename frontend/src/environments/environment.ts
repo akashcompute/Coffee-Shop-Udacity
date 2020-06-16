23b63e9465e5eaac191433fb5ef430fa65f4ee05
Akash Kumar <akashkrcse1@gmail.com>
2020-06-17 01:39:03 +0530

    Fixed Flask api server url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'dev-xen-841i.us.auth0.com', // the auth0 domain prefix
+    url: 'dev-xen-841i.us', // the auth0 domain prefix
     audience: 'coffee', // the audience set for the auth0 app
     clientId: 'H8s3oMWyjCjz8JhmnTHd6InPN7lHEe5Y', // the client id generated for the auth0 app
-    callbackURL: 'https://localhost:4200', // the base url of the running ionic application.
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
   }
 };
