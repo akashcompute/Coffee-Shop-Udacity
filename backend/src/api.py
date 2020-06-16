d46306ee44a89220a0f7999eca7b187599575c71
Akash Kumar <akashkrcse1@gmail.com>
2020-06-17 01:40:25 +0530

    Formatting Layout

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,9 +1,8 @@
 import os
-from flask import Flask, request, jsonify, abort
+from flask import (Flask, request, jsonify, abort)
 from sqlalchemy import exc
 import json
 from flask_cors import CORS
-
 from .database.models import db_drop_and_create_all, setup_db, Drink
 from .auth.auth import AuthError, requires_auth
 
